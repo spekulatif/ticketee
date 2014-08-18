7d990a25e2a6e512b97355cc6362ce9a39080caa
Ufuk Tandogan <ufuktandogan@windowslive.com>
2014-08-18 15:30:15 +0300

    We can nor edit tickets.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,19 +10,35 @@ before_action :set_ticket, only: [:show, :edit, :update, :destroy]
   @ticket = @project.tickets.build(ticket_params)
   if @ticket.save
     flash[:notice] = "Ticket has been created."
+    
     redirect_to [@project, @ticket]
   else
     flash[:alert] = "Ticket has not been created."
+    
     render "new"
   end
 
-  def set_ticket
-  	@ticket = @project.tickets.find(params[:id])
-	end
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      
+      render action: "edit"
+    end
+  end
 
+  def edit
+  end
 end 
 
 private
+
+  def set_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
 
   def ticket_params
     params.require(:ticket).permit(:title, :description)
