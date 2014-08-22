9e23b19f67335951674796d27c39b045728176d8
Ufuk Tandogan <ufuktandogan@windowslive.com>
2014-08-22 12:25:05 +0300

    Added admin flag to user

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,11 @@
-	FactoryGirl.define do
-	  factory :user do
-	    name "Example user"
-	    email "deneme@deneme.com"
-	    password "1234"
-	    password_confirmation "1234"
-	  end
+FactoryGirl.define do
+  factory :user do
+    name "username"
+    email "sample@example.com"
+    password "hunter2"
+    password_confirmation "hunter2"
+    factory :admin_user do
+      admin true
+		end 
 	end
+end
