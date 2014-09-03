99e5f8dabae6f56db8411c0ace6967037aeda74a
Ufuk Tandogan <ufuktandogan@windowslive.com>
2014-09-03 17:28:32 +0300

    Add feature for deleting users including protection aganist self-deletion

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,49 +1,60 @@
 class Admin::UsersController < Admin::BaseController
-	before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.order(:email)
   end
 
   def new
-  	@user = User.new
-	end
+    @user = User.new
+  end
 
-	def create
-	  params = user_params.dup
-	  params[:password_confirmation] = params[:password]
-	  @user = User.new(params)
-	  
-	  if @user.save
-	    flash[:notice] = "User has been created."
-	    redirect_to admin_users_path
-	  else
-	    flash.now[:alert] = "User has not been created."
-	    render :action => "new"
-		end 	
-	end
+  def create
+    params = user_params.dup
+    params[:password_confirmation] = params[:password]
+    @user = User.new(params)
+    
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been created."
+      render :action => "new"
+    end   
+  end
 
-	def show 
-	end
+  def show 
+  end
 
-	def edit 
-	end
+  def edit 
+  end
 
-	def update
-	  if @user.update(user_params)
-	    flash[:notice] = "User has been updated."
-	    redirect_to admin_users_path
-	  else
-	    flash[:alert] = "User has not been updated."
-	    render action: "edit"
-	  end
-	end
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been updated."
+      render action: "edit"
+    end
+  end
 
-	private
+  def destroy
+    if @user == current_user
+      flash[:alert] = "You cannot delete yourself!"
+    else
+      @user.destroy
+      flash[:notice] = "User has been deleted."
+    end
+   
+    redirect_to admin_users_path
+  end
+
+  private
   
   def set_user
     @user = User.find(params[:id])
-	end
+  end
   
   def user_params
     params.require(:user).permit(:name,:email,:password,
