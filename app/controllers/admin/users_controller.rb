33444bc75efd8a54feea7efb5d65654f1b8057e3
Ufuk Tandogan <ufuktandogan@windowslive.com>
2014-09-03 11:00:52 +0300

    Add the ability to create admin users through the admin backend

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -24,7 +24,9 @@ class Admin::UsersController < Admin::BaseController
 private
   def user_params
     params.require(:user).permit(:name,
+    														 :email,
                                  :password,
-                                 :password_confirmation)
+                                 :password_confirmation,
+                                 :admin)
   end
 end
